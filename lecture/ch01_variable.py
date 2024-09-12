286500d3cf51f7c76192b0d5c3e02e4bed689218
박경현 <baggyeonghyeon@baggyeonghyeon-ui-MacBookAir.local>
2024-09-12 17:29:26 +0900

    ss

---
+++ lecture/ch01_variable.py
@@ -0,0 +1,36 @@
+print("hello")
+print('hello')
+# 1. 출력함수(print)
+# - () 값을 출력
+# - 변수값 확인 용도로 많이 활용
+# c언어
+# - ''(char) - 문자형
+# - ""(string) - 문자열형
+# - \t 탭
+# - \n 한줄 개행
+
+
+print("hello \tworld")
+print("hello \nworld")
+
+# 3 자료형(Type)
+"""
+정수형int - 1,2,4
+실수형float - 3.14 0.0
+문자열String - "Hi"
+논리형Boolean - True, False
+"""
+
+# 4.형변환
+"""
+원하는 자료형으로 변환
+int()
+float()
+str()
+"""
+
+# 5. 변수선언
+"""
+num = 5
+type을 정의하지 않음 - 동적 타이핑 언어
+"""
